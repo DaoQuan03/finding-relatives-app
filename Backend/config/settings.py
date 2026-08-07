30ff34f459ec959ec416ee407b5911af4eb4ec97
DaoQuan03 <daoquan20032005@gmail.com>
2026-08-07 21:12:05 +0700

    Add Django apps (accounts, core, matching, person, post) and environment config

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -37,6 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
+
+    'accounts',
+    'post',
+    'matching',
+    'core',
+    'person'
+    
 ]
 
 MIDDLEWARE = [
@@ -74,8 +88,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'finding_relatives_db',
+        'USER': 'frdb_user',
+        'PASSWORD': 'matkhau_manh',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
